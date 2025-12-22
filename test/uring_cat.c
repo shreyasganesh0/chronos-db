8db30c31a5348e7c7793ee22eb16558c42db9616
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2025-12-22 09:23:46 -0500

    feat: create sring and cring setup function

--- test/uring_cat.c
+++ test/uring_cat.c
@@ -46,6 +46,18 @@ struct file_info {
     struct iovec iovecs[];
 };
 
+int io_uring_setup(unsigned entries, struct io_uring_params *p) {
+
+    return (int) syscall(__NR_io_uring_setup, entries, p); 
+}
+
+int io_uring_enter(int ring_fd, unsigned int to_submit,
+        unsigned int min_complete, unsigned int flags) {
+
+    return (int) syscall(__NR_io_uring_enter, ring_fd, to_submit, 
+            min_complete, flags, NULL, 0); 
+}
+
 off_t get_file_size(int fd) {
 
     struct stat st;
@@ -68,4 +80,60 @@ off_t get_file_size(int fd) {
     }
 
     return -1;
+}
+
+int app_setup_uring(struct submitter *s) {
+
+    struct app_io_sq_ring *sring = &s->sq_ring;
+    struct app_io_cq_ring *cring = &s->cq_ring;
+    struct io_uring_params p;
+    void *sq_ptr, *cq_ptr;
+
+    memset(&p, 0, sizeof(p));
+    s->ring_fd = io_uring_setup(QUEUE_DEPTH, &p);
+    if (s->ring_fd < 0) {
+        perror("io_uring_setup");
+        return 1;
+    }
+
+    int sring_sz = p.sq_off.array + p.sq_entries * sizeof(unsigned);
+    int cring_sz = p.cq_off.cqes + p.cq_entries * sizeof(struct io_uring_cqe);
+
+    // only do 1 mmap if above kernel 5.4 version by checking bit flag
+    if (p.features & IORING_FEAT_SINGLE_MMAP) {
+        if (cring_sz > sring_sz) {
+            sring_sz = cring_sz;
+        }
+        cring_sz = sring_sz;
+    }
+
+    sq_ptr = mmap(0, sring_sz, PROT_READ | PROT_WRITE,
+            MAP_SHARED | MAP_POPULATE,
+            s->ring_fd, IORING_OFF_SQ_RING);
+
+    if (sq_ptr == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    if (p.features & IORING_FEAT_SINGLE_MMAP) {
+        cq_ptr = sq_ptr;
+    } else {
+
+        cq_ptr = mmap(0, cring_sz, PROT_READ | PROT_WRITE,
+                MAP_SHARED | MAP_POPULATE,
+                s->ring_fd, IORING_OFF_CQ_RING);
+        if (cq_ptr == MAP_FAILED) {
+            perror("mmap");
+            return 1;
+        }
+    }
+
+    cring->head = cq_ptr + p.cq_off.head;
+    cring->tail = cq_ptr + p.cq_off.tail;
+    cring->ring_mask = cq_ptr + p.cq_off.ring_mask;
+    cring->ring_entries = cq_ptr + p.cq_off.ring_entries;
+    cring->cqes = cq_ptr + p.cq_off.cqes;
+
+    return 0;
 }
